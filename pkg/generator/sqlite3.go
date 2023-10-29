34f050129b58a1ba162fe27d7b07e6cd95422fad
Milos Zivlak <zivlakmilos@gmail.com>
2023-10-29 16:56:41 +0100

    Sqlite3 generator

---
+++ pkg/generator/sqlite3.go
@@ -0,0 +1,153 @@
+package generator
+
+import (
+	"fmt"
+	"io"
+
+	"github.com/gophoria/gophoria/pkg/ast"
+)
+
+var sqlite3Types = map[ast.VariableType][]byte{
+	ast.VariableTypeInt:      []byte("INTEGER"),
+	ast.VariableTypeReal:     []byte("REAL"),
+	ast.VariableTypeBool:     []byte("INTEGER"),
+	ast.VariableTypeString:   []byte("TEXT"),
+	ast.VariableTypeDateTime: []byte("TEXT"),
+}
+
+type Sqlite3Generator struct {
+	ast    *ast.Ast
+	writer io.Writer
+}
+
+func NewSqlite3Generator() *Sqlite3Generator {
+	g := Sqlite3Generator{}
+
+	return &g
+}
+
+func (g *Sqlite3Generator) GenerateAll(ast *ast.Ast, writer io.Writer) error {
+	g.ast = ast
+	g.writer = writer
+
+	for _, model := range ast.Models {
+		err := g.generateModel(model)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (g *Sqlite3Generator) Generate(ast *ast.Ast, name string, writer io.Writer) error {
+	g.ast = ast
+	g.writer = writer
+
+	isExist := false
+
+	for _, model := range ast.Models {
+		if model.Name.Identifier == name {
+			isExist = true
+
+			err := g.generateModel(model)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	if !isExist {
+		return fmt.Errorf("model %s not found", name)
+	}
+
+	return nil
+}
+
+func (g *Sqlite3Generator) generateModel(model *ast.Model) error {
+	g.writer.Write([]byte("CREATE TABLE IF NOT EXISTS "))
+	g.writer.Write([]byte(model.Name.Token.Literal))
+	g.writer.Write([]byte(" (\n"))
+
+	for _, item := range model.Items {
+		err := g.generateItem(item)
+		if err != nil {
+			return err
+		}
+	}
+
+	g.writer.Write([]byte(")\n\n"))
+
+	return nil
+}
+
+func (g *Sqlite3Generator) generateItem(item *ast.Declaration) error {
+	decType, ok := g.typeToSqliteType(item.DeclarationType)
+	if !ok {
+		if g.isTypeModel(item.DeclarationType) {
+			return nil
+		} else {
+			return fmt.Errorf("invalid type %s", item.DeclarationType.Name)
+		}
+	}
+
+	g.writer.Write([]byte("  "))
+	g.writer.Write([]byte(item.Identifier.Identifier))
+	g.writer.Write([]byte(" "))
+	g.writer.Write(decType)
+
+	if _, ok := g.getDecorator("id", item.Decorators); ok {
+		g.writer.Write([]byte(" PRIMARY KEY"))
+	}
+
+	g.writer.Write([]byte("\n"))
+
+	return nil
+}
+
+func (g *Sqlite3Generator) typeToSqliteType(decType *ast.DeclarationType) ([]byte, bool) {
+	sqlType, ok := sqlite3Types[decType.Type]
+	if ok {
+		return sqlType, true
+	}
+
+	name := decType.Name
+
+	for _, enum := range g.ast.Enums {
+		if enum.Name.Identifier == name {
+			if len(enum.Items) > 0 {
+				enumType := enum.Items[0].Value.Type
+				if enumType == ast.ValueTypeString {
+					return []byte("TEXT"), true
+				}
+				if enumType == ast.ValueTypeInt {
+					return []byte("INTEGER"), true
+				}
+			}
+		}
+	}
+
+	return []byte{}, false
+}
+
+func (g *Sqlite3Generator) isTypeModel(decType *ast.DeclarationType) bool {
+	name := decType.Name
+
+	for _, model := range g.ast.Models {
+		if model.Name.Identifier == name {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (g *Sqlite3Generator) getDecorator(name string, decorators []*ast.Decorator) (*ast.Decorator, bool) {
+	for _, dec := range decorators {
+		if dec.Name.Identifier == name {
+			return dec, true
+		}
+	}
+
+	return nil, false
+}
---
+++ pkg/generator/sqlite3_test.go
@@ -0,0 +1,77 @@
+package generator_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/gophoria/gophoria/pkg/generator"
+	"github.com/gophoria/gophoria/pkg/lexer"
+	"github.com/gophoria/gophoria/pkg/parser"
+)
+
+func TestSqlite3Generator(t *testing.T) {
+	input := `
+db {
+  provider = "sqlite3"
+  url = ":memory:"
+}
+
+enum Role {
+  admin = "admin"
+  user = "user"
+}
+
+model User {
+  id      string  @id @default(uuid())
+  name    string
+  surname string
+  role    Role
+  posts   Post[]
+}
+
+model Post {
+  id        int     @id @default(autoincrement())
+  title     string
+  content   string  @nullable
+  public    bool    @default(false)
+  author    User    @relation(field: authorId, reference: id)
+  authorId  int
+}`
+
+	expected := `CREATE TABLE IF NOT EXISTS User (
+  id TEXT PRIMARY KEY
+  name TEXT
+  surname TEXT
+  role TEXT
+)
+
+CREATE TABLE IF NOT EXISTS Post (
+  id INTEGER PRIMARY KEY
+  title TEXT
+  content TEXT
+  public INTEGER
+  authorId INTEGER
+)
+
+`
+
+	lexer := lexer.NewLexer(input)
+	parser := parser.NewParser(lexer)
+
+	ast, err := parser.Parse()
+	if err != nil {
+		t.Fatalf("parser error: %s", err.Error())
+	}
+
+	generator := generator.NewSqlite3Generator()
+
+	var buffer bytes.Buffer
+	err = generator.GenerateAll(ast, &buffer)
+	if err != nil {
+		t.Fatalf("generator error: %s", err.Error())
+	}
+
+	if buffer.String() != expected {
+		t.Fatalf("Generator output is not correct")
+	}
+}
