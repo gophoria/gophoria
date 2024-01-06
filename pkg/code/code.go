b5fb3e4635ddf5457c969e09b063b7b3ec01381c
Asaadziad <asaadabuelhija@gmail.com>
2024-01-06 09:43:10 +0200

    refactored generateDateTime function

---
+++ pkg/code/code.go
@@ -0,0 +1,51 @@
+package code
+
+type Code struct {
+	CodeString string
+}
+
+func GetCodeStringAsBytes(c Code) []byte {
+	return []byte(c.CodeString)
+}
+
+// DateTime file string
+var DT = Code{CodeString: `
+package db
+import (
+"database/sql/driver"
+"fmt"
+"time"
+)
+
+type DateTime time.Time
+
+func Now() DateTime {
+ return DateTime(time.Now())
+}
+
+func (d DateTime) Value() (driver.Value, error) {
+	t := time.Time(d)
+	return t.Format(time.RFC3339), nil
+}
+
+func (d *DateTime) Scan(src interface{}) error {
+	var source string
+	switch src := src.(type) {
+		case string:
+			source = src
+		case []byte:
+			source = string(src)
+		default:
+			return fmt.Errorf("incompatible type")
+	}
+
+	dateTime, err := time.Parse(time.RFC3339, source)
+	if err != nil {
+		return err
+	}
+
+	*d = DateTime(dateTime)
+
+	return nil
+}
+`}
--- pkg/generator/sqlx.go
+++ pkg/generator/sqlx.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gophoria/gophoria/internal/utils"
 	"github.com/gophoria/gophoria/pkg/ast"
+	"github.com/gophoria/gophoria/pkg/code"
 )
 
 type SqlxGenerator struct {
@@ -182,46 +183,7 @@ func (g *SqlxGenerator) generateEnum(enum *ast.Enum) error {
 }
 
 func (g *SqlxGenerator) generateDateTime(ast *ast.Ast, writer io.Writer) error {
-	writer.Write([]byte("package db\n\n"))
-
-	writer.Write([]byte("import (\n"))
-	writer.Write([]byte("\t\"database/sql/driver\"\n"))
-	writer.Write([]byte("\t\"fmt\"\n"))
-	writer.Write([]byte("\t\"time\"\n"))
-	writer.Write([]byte(")\n\n"))
-
-	writer.Write([]byte("type DateTime time.Time\n\n"))
-
-	writer.Write([]byte("func Now() DateTime {\n"))
-	writer.Write([]byte("\treturn DateTime(time.Now())\n"))
-	writer.Write([]byte("}\n\n"))
-
-	writer.Write([]byte("func (d DateTime) Value() (driver.Value, error) {\n"))
-	writer.Write([]byte("\tt := time.Time(d)\n"))
-	writer.Write([]byte("\treturn t.Format(time.RFC3339), nil\n"))
-	writer.Write([]byte("}\n\n"))
-
-	writer.Write([]byte("func (d *DateTime) Scan(src interface{}) error {\n"))
-	writer.Write([]byte("\tvar source string\n\n"))
-
-	writer.Write([]byte("\tswitch src := src.(type) {\n"))
-	writer.Write([]byte("\tcase string:\n"))
-	writer.Write([]byte("\t\tsource = src\n"))
-	writer.Write([]byte("\tcase []byte:\n"))
-	writer.Write([]byte("\t\tsource = string(src)\n"))
-	writer.Write([]byte("\tdefault:\n"))
-	writer.Write([]byte("\t\treturn fmt.Errorf(\"incopatible type\")\n"))
-	writer.Write([]byte("\t}\n\n"))
-
-	writer.Write([]byte("\tdateTime, err := time.Parse(time.RFC3339, source)\n"))
-	writer.Write([]byte("\tif err != nil {\n"))
-	writer.Write([]byte("\t\treturn err\n"))
-	writer.Write([]byte("\t}\n\n"))
-
-	writer.Write([]byte("\t*d = DateTime(dateTime)\n\n"))
-
-	writer.Write([]byte("\treturn nil\n"))
-	writer.Write([]byte("}\n"))
+	writer.Write(code.GetCodeStringAsBytes(code.DT))
 
 	return nil
 }
