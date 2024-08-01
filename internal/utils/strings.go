8e0b12c377e84e96167cc44eeb25645efc5f8597
Milos Zivlak <zivlakmilos@gmail.com>
2024-08-02 01:39:54 +0200

    String utils

--- internal/utils/strings.go
+++ internal/utils/strings.go
@@ -1,7 +1,14 @@
 package utils
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+)
 
 func Capitalize(str string) string {
 	return fmt.Sprintf("%c%s", str[0]-32, str[1:])
+}
+
+func WriteString(writer io.Writer, str string) (int, error) {
+	return writer.Write([]byte(str))
 }
